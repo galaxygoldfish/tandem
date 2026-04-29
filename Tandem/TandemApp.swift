03273619e09842159ad0b3fec5e6b4bf8b64bb09
sebastian <sebastianhriscu25@gmail.com>
2026-04-28 17:05:39 -0700

    Initial Commit

---
+++ Tandem/TandemApp.swift
@@ -0,0 +1,17 @@
+//
+//  TandemApp.swift
+//  Tandem
+//
+//  Created by Sebastian Hriscu on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TandemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
